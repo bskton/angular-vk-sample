fb8adb2203dcb5ab025cc7f74d7e37d34f054360
Ilya Levin <bskton@gmail.com>
2018-10-28 00:52:38 +1000

    Unsubscribing from user and friends changes in home component

--- src/app/application/home/home.component.ts
+++ src/app/application/home/home.component.ts
@@ -1,30 +1,48 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { take, switchMap } from "rxjs/operators";
 
 import { UserService } from "../../domain/model/user/user.service";
 import { User } from "../../domain/model/user/user";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   user: User;
 
   friends: User[];
 
+  private userSubscription: Subscription;
+
+  private friendsSubscription: Subscription;
+
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.userChanged().subscribe((user: User) => {
+    this.userSubscription = this.userService
+      .userChanged()
+      .subscribe((user: User) => {
         this.user = user;
-    });
+      });
     this.userService.getUser();
-    this.userService.friendsChanged().subscribe((friends: User[]) => {
-      this.friends = friends;
-    })
+    this.friendsSubscription = this.userService
+      .friendsChanged()
+      .subscribe((friends: User[]) => {
+        this.friends = friends;
+      });
     this.userService.getFiveFriends();
+  }
+
+  ngOnDestroy() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+    if (this.friendsSubscription) {
+      this.friendsSubscription.unsubscribe();
+    }
   }
 
   isAuthenticated(): boolean {
